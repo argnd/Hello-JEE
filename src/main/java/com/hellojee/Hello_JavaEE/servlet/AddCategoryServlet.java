fb9abc193f54a302b72ffe383553a6aca36aef66
argnd <gandour37@gmail.com>
2021-06-10 17:11:12 +0200

    651

--- src/main/java/com/hellojee/Hello_JavaEE/servlet/AddCategoryServlet.java
+++ src/main/java/com/hellojee/Hello_JavaEE/servlet/AddCategoryServlet.java
@@ -2,13 +2,9 @@ package com.hellojee.Hello_JavaEE.servlet;
 
 import com.hellojee.Hello_JavaEE.entity.Category;
 
-<<<<<<< Updated upstream
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
-import javax.servlet.ServletException;
-=======
->>>>>>> Stashed changes
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -31,7 +27,6 @@ public class AddCategoryServlet extends HttpServlet {
     }
 
     @Override
-<<<<<<< Updated upstream
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         EntityManager em = this.emf.createEntityManager();
         Category c = new Category();
@@ -42,15 +37,5 @@ public class AddCategoryServlet extends HttpServlet {
         em.close();
 
         resp.sendRedirect(req.getContextPath()+"/Vues/Welcome.jsp");
-    }
-
-    @Override
-    public void destroy() {
-        this.emf.close();
-=======
-    protected void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException {
-        OtherDaoFactory.getMyProductDao().create(new Category());
-        res.sendRedirect(req.getContextPath()+"/");
->>>>>>> Stashed changes
     }
 }
